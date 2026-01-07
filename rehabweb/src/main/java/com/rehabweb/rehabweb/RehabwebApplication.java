d66b7d2aef82a098fe03510f406fe7fd58e48033
hydrian634 <hydrian634@naver.com>
2026-01-07 14:23:15 +0900

    feat. spring project created

---
+++ rehabweb/src/main/java/com/rehabweb/rehabweb/RehabwebApplication.java
@@ -0,0 +1,13 @@
+package com.rehabweb.rehabweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehabwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RehabwebApplication.class, args);
+	}
+
+}
---
+++ rehabweb/src/main/java/com/rehabweb/rehabweb/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.rehabweb.rehabweb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RehabwebApplication.class);
+	}
+
+}
